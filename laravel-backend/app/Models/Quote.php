f2705c6d2f46690fd1e1799fd825f1075117119f
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2023-07-18 00:19:52 +0600

    make project and /api/quote route

---
+++ laravel-backend/app/Models/Quote.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quote extends Model
+{
+    protected $fillable = ['text', 'author', 'date'];
+}
